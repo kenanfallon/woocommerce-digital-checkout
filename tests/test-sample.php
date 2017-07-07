f884d50f99e9326615d21119466b6559aa3da157
Kenan Fallon <kenan@kenanfallon.com>
2017-07-08 09:39:29 +1000

    require PHP7 & PHP7.1, add WooCommerce to our test, added simple WooCommerce test.

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -17,4 +17,15 @@ class SampleTest extends WP_UnitTestCase {
 		// Replace this with some actual testing code.
 		$this->assertTrue( true );
 	}
+
+	/**
+	 * Test that relies on WooCommerce
+	 */
+	function test_woocommerce() {
+		// Assuming the product actually exists :)
+		$product_id = 666;
+		WC()->cart->add_to_cart( $product_id );
+
+		$this->assertNotEmpty( WC()->cart->cart_contents );
+	}
 }
